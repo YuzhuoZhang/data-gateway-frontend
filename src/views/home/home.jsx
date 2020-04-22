091874772f8dae42105fb4fc4ed2d1dabd3b24c5
zyz <1415068436@qq.com>
2020-04-22 17:01:12 +0800

    4/22

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -97,6 +97,7 @@ export default class Home extends React.Component {
                             {/* 主体部分的路由 */}
                             <Switch>
                                 <Route path='/home' component={Start}/>
+                                <Route path='/updatePwd' component={}/>
                                 <Route path='/dgs/dss' component={DataSourceSearch}/>
                                 {/* <Route path='/dgs/is' component={}/> */}
                                 <Route path='/dgm/dsm' component={DataSourceMange}/>
